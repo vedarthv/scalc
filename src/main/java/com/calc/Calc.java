49ad70ff9c5b9bf6ca32e3a913ad681dd219827c
Vedarth V <vedarth1@gmail.com>
2023-01-17 22:37:52 -0500

    Cleaned up visitor methods
    - abstracted away the operations to a single function

--- src/main/java/com/calc/Calc.java
+++ src/main/java/com/calc/Calc.java
@@ -11,17 +11,13 @@ public class Calc {
 
     public static void main(String[] args) throws Exception {
         // create a CharStream that reads from standard input
-        //ANTLRInputStream input = new ANTLRInputStream(System.in); // create a lexer that feeds off of input CharStream
-        CharStream charStream = CharStreams.fromString("(add 1 1 1 (multiply 2 6) (exponent 3 1))");
+        CharStream charStream = CharStreams.fromString(args[0]);
         CalcLexer lexer = new CalcLexer(charStream); // create a buffer of tokens pulled from the lexer
         CommonTokenStream tokens = new CommonTokenStream(lexer); // create a parser that feeds off the tokens buffer
         CalcParser parser = new CalcParser(tokens);
-        ParseTreeVisitor visitor = new CalcVisitor();
-        int result = (int) visitor.visit(parser.expr());
-
-        System.out.println(result); // print LISP-style tree
-
-
+        ParseTreeVisitor visitor = new CalcVisitor();  // We will use our extended baseVisitor class to perform operations
+        int result = (int) visitor.visit(parser.expr()); // Our Visitor class is passed a parse tree, the visitor class then performs the appropriate ops
+        System.out.println(result);
     }
 
 
--- src/main/java/com/calc/CalcVisitor.java
+++ src/main/java/com/calc/CalcVisitor.java
@@ -4,74 +4,57 @@ import java.util.*;
 
 import com.antlr.CalcBaseVisitor;
 import com.antlr.CalcParser;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 public class CalcVisitor extends CalcBaseVisitor<Integer> {
 
+    private Integer Operation(List<ParseTree> expression){
+        int result = -1; // initializing result to dummy value
+        String OP = expression.get(1).getText();
+        // We start at i=3 to ignore the left bracket, op, delim tokens in a (non-int) expression context.
+        for (int i = 3; i < expression.size(); i++) {
+            if (expression.get(i).getChildCount() >= 1){
+                // Here the i'th child (i >= 3), the object must be an int since a non int expression has > 1 children.
+                if (result == -1){
+                    // We re-initialize the result to its proper value
+                    result = visit(expression.get(i));
+                }
+                else {
+                    switch (OP) {
+                        case "exponent":
+                            result = (int) Math.pow(result, visit(expression.get(i)));
+                            break;
+                        case  "multiply":
+                            result *= visit(expression.get(i));
+                            break;
+                        case "add":
+                            result += visit(expression.get(i));
+                            break;
+                        default:
+                            throw new RuntimeException("Invalid OP");
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
     @Override public Integer visitProgram(CalcParser.ProgramContext ctx) {
         int value = visit(ctx); // evaluate the expr child
-        System.out.println(value); // print the result
         return value;
     }
 
     @Override public Integer visitExponent(CalcParser.ExponentContext ctx) {
-        int result = -1; // initializing result to dummy value
-
-        int startIndex = 3; // We start at i=3 to ignore the left bracket, op, delim tokes in a (non-int) expression context.
-        for (int i = 3; i < ctx.getChildCount(); i++) {
-
-            if (ctx.children.get(i).getChildCount() >= 1){
-                // Here the i'th child (i >= 3), the object must be an int since a non int expression has > 1 children.
-                if (result == -1){
-                    // We re-initialize the result to its proper value
-                    result = visit(ctx.getChild(i));
-                }
-                else {
-                    result = (int) Math.pow(result, visit(ctx.getChild(i)));
-                }
-            }
-        }
-        return result;
+        return Operation(ctx.children);
     }
 
 
     @Override public Integer visitMultiply(CalcParser.MultiplyContext ctx) {
-        int result = -1; // initializing result to dummy value
-
-        int startIndex = 3; // We start at i=3 to ignore the left bracket, op, delim tokes in a (non-int) expression context.
-        for (int i = 3; i < ctx.getChildCount(); i++) {
-
-            if (ctx.children.get(i).getChildCount() >= 1){
-                // Here the i'th child (i >= 3), the object must be an int since a non int expression has > 1 children.
-                if (result == -1){
-                    // We re-initialize the result to its proper value
-                    result = visit(ctx.getChild(i));
-                }
-                else {
-                    result *= visit(ctx.getChild(i));
-                }
-            }
-        }
-        return result;
+        return Operation(ctx.children);
     }
 
     @Override public Integer visitAdd(CalcParser.AddContext ctx) {
-        int result = -1; // initializing result to dummy value
-
-        int startIndex = 3; // We start at i=3 to ignore the left bracket, op, delim tokes in a (non-int) expression context.
-        for (int i = 3; i < ctx.getChildCount(); i++) {
-
-            if (ctx.children.get(i).getChildCount() >= 1){
-                // Here the i'th child (i >= 3), the object must be an int since a non int expression has > 1 children.
-                if (result == -1){
-                    // We re-initialize the result to its proper value
-                    result = visit(ctx.getChild(i));
-                }
-                else {
-                    result += visit(ctx.getChild(i));
-                }
-            }
-        }
-        return result;
+        return Operation(ctx.children);
     }
 
     @Override public Integer visitInteger(CalcParser.IntegerContext ctx) { return Integer.valueOf(ctx.INT().getText()); }
